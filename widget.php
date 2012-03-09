20b393f510af2faa171ea6796f09766dbff1882a
Vinicius Depizzol <vdepizzol@gmail.com>
2012-03-08 23:13:52 -0300

    made widget support new html5 format for wordpress (and avoid duplicating the default language item)

--- widget.php
+++ widget.php
@@ -7,18 +7,15 @@ wp_register_sidebar_widget(
         
         $langs = wppo_get_all_available_langs();
         
-        echo '<li id="languages" class="widget-container widget_languages">'."\n".
+        echo '<aside id="languages" class="widget widget_languages">'."\n".
              '<h3 class="widget-title">'.__('Languages').'</h3>'."\n".
              '<ul>'."\n";
-        
-        
-        echo '<li><a href="'.wppo_recreate_url($_SERVER['REQUEST_URI'], WPPO_DEFAULT_LANGUAGE_CODE).'">'.WPPO_DEFAULT_LANGUAGE_NAME.'</a></li>';
         
         foreach ($langs as $lang_code => $lang_name) {
             echo '<li><a href="'.wppo_recreate_url($_SERVER['REQUEST_URI'], $lang_code).'">'.$lang_name.'</a></li>';
         }
         echo '</ul>'."\n".
-             '</li>'."\n";
+             '</aside>'."\n";
     },
     array(
         'description' => 'List all available languages'
